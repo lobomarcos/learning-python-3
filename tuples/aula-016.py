47075c525f7071440f0c325c1ffa82a586b9e6de
lobomarcosemail <wofeworks@gmail.com>
2025-07-29 21:19:30 -0300

    INITIAL

---
+++ tuples/aula-016.py
@@ -0,0 +1,4 @@
+# TUPLAS - são variáveis compostas e imutáveis que permitem armazenar vários valores em uma mesma estrutura que podem ser acessadas por chaves individuais.
+# É UMA VARIÁVEL QUE GUARDA VÁRIOS VALORES.
+
+# PAREI EM 20 MIN
