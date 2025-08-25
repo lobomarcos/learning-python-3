28f8178710c0b597c0d4cc9e610453e6fe0be9df
lobomarcosemail <wofeworks@gmail.com>
2025-08-25 17:49:24 -0300

    ###

--- tuples/D01.py
+++ tuples/D01.py
@@ -1,3 +1,3 @@
-numbers = ['Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete,', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte']
+numberList = ['Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete,', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte']
 
-int = input ('Qual número você quer escrever por extenso? Digíte um número de 0 a 20.')
+numberInput = int (input('Qual número você quer escrever por extenso? Digíte um número de 0 a 20.'))
