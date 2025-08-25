1f92667dd6a53e5cfeb5a1c76ec1d1bf79e11c83
lobomarcosemail <wofeworks@gmail.com>
2025-08-25 17:47:51 -0300

    initial

---
+++ tuples/D01.py
@@ -0,0 +1,3 @@
+numbers = ['Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete,', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte']
+
+int = input ('Qual número você quer escrever por extenso? Digíte um número de 0 a 20.')
--- tuples/aula-016.py
+++ tuples/aula-016.py
@@ -1,4 +1,9 @@
 # TUPLAS - são variáveis compostas e imutáveis que permitem armazenar vários valores em uma mesma estrutura que podem ser acessadas por chaves individuais.
 # É UMA VARIÁVEL QUE GUARDA VÁRIOS VALORES.
+# Tuplas podem ser criadas sem parênteses.
 
+# Ex. 1:
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print (lanche[1])
 # PAREI EM 20 MIN
