ce78c2ca4425498ea96e5b3e9c57d6bb4deefe7e
lobomarcosemail <wofeworks@gmail.com>
2025-08-26 03:02:38 -0300

    exercicio 1

--- tuples/D01.py
+++ tuples/D01.py
@@ -1,3 +1,9 @@
-numberList = ['Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete,', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte']
+cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete,', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 
-numberInput = int (input('Qual número você quer escrever por extenso? Digíte um número de 0 a 20.'))
+while True:
+  num = int (input('Qual número você quer escrever por extenso? Digíte um número de 0 a 20: '))
+  if 0 <= num <= 20:
+    break
+  print ('Tente novamente.')
+
+print ('Você digitou o número: {}.' .format(cont[num]))
