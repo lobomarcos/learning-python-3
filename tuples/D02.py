cac72a3749570f0d23a0b0fdd1d59387dd9e63cc
lobomarcosemail <wofeworks@gmail.com>
2025-08-26 03:33:23 -0300

    exercicio2

---
+++ tuples/D02.py
@@ -0,0 +1,7 @@
+table = ('Flamengo', 'Palmeiras', 'Cruzeiro', 'Bahia', 'Botafogo', 'Mirassol', 'São Paulo', 'Bragantino', 'Fluminense', 'Ceará', 'Corinthians', 'Atlético-MG', 'Internacional', 'Grêmio', 'Santos', 'Vasco', 'Vitória', 'Juventude', 'Fortaleza', 'Sport')
+
+print ('Times do Brasileirão: {}.' .format(table))
+print ('Os primeiros 5 colocados são {}.' .format(table[0:5]))
+print ('Os 4 últimos colocados são {}.' .format(table[16:21]))
+print ('A ordem alfabética da tabela do Brasileirão é: {}.' .format(sorted(table)))
+print ('O Mirassol está em 6º lugar na tabela do Brasileirão.')
